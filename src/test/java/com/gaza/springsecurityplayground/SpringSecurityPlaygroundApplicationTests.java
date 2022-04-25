3c3e991b9ec5ddbee70109d4300a4833c44c67f5
Nihad <ngazic@gmail.com>
2022-04-25 23:44:19 +0200

    initial commit

---
+++ src/test/java/com/gaza/springsecurityplayground/SpringSecurityPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaza.springsecurityplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
